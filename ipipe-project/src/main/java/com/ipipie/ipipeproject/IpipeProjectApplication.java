f93570569a03e753aac284c6cbd25d831f240650
Rafael Victor de Oliveira <rafaelvictor368@hotmail.com>
2022-05-14 22:32:07 -0300

    Start Spring

---
+++ ipipe-project/src/main/java/com/ipipie/ipipeproject/IpipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ipipie.ipipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpipeProjectApplication.class, args);
+	}
+
+}
---
+++ ipipe-project/src/test/java/com/ipipie/ipipeproject/IpipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipipie.ipipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
